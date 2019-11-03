9fc7dbb3d1c412ec82f83c031c1b7163e0d5c30e
rhezaziz <rheza.khairullahaziz10@gmail.com>
2019-11-04 02:10:57 +0700

    Login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'validasi.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'network_util.dart';
 //import 'package:login/home_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,16 +12,55 @@ class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => new _LoginPageState();
 }
-
+enum LoginStatus { notSignIn, signIn }
 class _LoginPageState extends State<LoginPage> with Validasi{
+  LoginStatus _loginStatus = LoginStatus.notSignIn;
   final formKey = GlobalKey<FormState>();
 
   String emailInput = '';
   String passwordInput = '';
+   bool _secureText = true;
 
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+  check() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      login();
+    }
+  }
+  login() async {
+    final response = await http.post(BaseUrl.login,
+        body: {"nim": emailInput, "password": passwordInput});
+    final data = jsonDecode(response.body);
+    int value = data['value'];
+    String pesan = data['message'];
+    
+    if (value == 1) {
+      setState(() {
+        _loginStatus = LoginStatus.signIn;
+       
+      });
+      print(pesan);
+    } else {
+      print(pesan);
+    }
+  }
+  
+  @override
+  void initState() {
+ 
+    super.initState();
+   
+  }
   @override
   Widget build(context) {
-    
+    switch(_loginStatus){
+      case LoginStatus.notSignIn:
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
@@ -28,56 +71,74 @@ class _LoginPageState extends State<LoginPage> with Validasi{
             Image.asset('asset/logosinsen.png', width: 70.0 , height: 70.0,
         ),
             SizedBox(height: 40.0),
-            email(),
+            emailVoid(),
             SizedBox(height: 10.0),
-            password(),
+            passwordVoid(),
             SizedBox(height: 24.0),
             loginButton(),
           ],
         )
       ),
     );
-
+    break;
+    case LoginStatus.signIn:
+        // return HomePage();
+        return HomePage();
+        break;
+    }
   }
   
 
 
     //
-      Widget email(){
+      Widget emailVoid(){
         return TextFormField(
-      keyboardType: TextInputType.emailAddress,
+      
+      keyboardType: TextInputType.number,
       autofocus: false,
       initialValue: '',
       decoration: InputDecoration(
-        labelText: 'Email',
-        hintText: 'Masukkan Email',
+        labelText: 'NIM',
+        hintText: 'Masukkan NIM',
          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
-      validator: validasiEmail,
-      onSaved: (String value){
-        emailInput = value;
+      validator: (e){
+        if (e.isEmpty) {
+                      return "Tolong Masukkan NIM";
+                    }
       },
+    onSaved: (e) => emailInput = e,
     );
   }
   
 
    
-      Widget password(){
+      Widget passwordVoid(){
         return TextFormField(
       autofocus: false,
       initialValue: '',
-      obscureText: true,
+      obscureText: _secureText,
+      
       decoration: InputDecoration(
         labelText: 'Password',
         hintText: 'Masukkan Password',
        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+       suffixIcon: IconButton(
+                      onPressed: showHide,
+                      icon: Icon(_secureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+       
       ),
-      validator: validasiPassword,
-      onSaved: (String value){
-        passwordInput = value;
+      validator: (e){
+        if (e.isEmpty) {
+                      return "Tolong Masukkan Password";
+                    }
       },
+      onSaved: (e) => passwordInput = e,
     );
   }
   // 
@@ -92,12 +153,7 @@ class _LoginPageState extends State<LoginPage> with Validasi{
           minWidth: 200.0,
           height: 42.0,
           onPressed: () {
-            if (formKey.currentState.validate()) {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()),);
-          
-          formKey.currentState.save();
-  
-            }
+            check();
           },
           color: Colors.lightBlueAccent,
           child: Text('Login', style: TextStyle(color: Colors.white)),
